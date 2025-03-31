edc3dd55de2a8641828214adde5303a5e8a49990
Marco D'Elia <dlemrc.digital@gmail.com>
2025-03-31 23:28:07 +0200

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = (config) => {
+    config.addPassthroughCopy('./src/img/');
+    config.addPassthroughCopy('./src/css/');
+    // Returns work items, sorted by display order
+config.addCollection('bricks', (collection) => {
+	return collection
+		.getFilteredByGlob('./src/bricks/*.md')
+		.sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+});
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
